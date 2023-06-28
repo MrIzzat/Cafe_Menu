a5715bc9b50d5345fe384982b444307767e4e955
MrIzzat <guineapigsarecute3748@gmail.com>
2023-06-29 01:43:55 +0300

    Splash Screen Action Bar

--- app/src/main/java/com/example/cafemenu/SplashScreen.java
+++ app/src/main/java/com/example/cafemenu/SplashScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -22,12 +23,13 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setupViews();
 
         topLeft= AnimationUtils.loadAnimation(this,R.anim.topleft_animation);
         bottomRight = AnimationUtils.loadAnimation(this,R.anim.bottomright_animation);
-
-
 
         imgLogo.setAnimation(topLeft);
         txtLogo.setAnimation(bottomRight);
